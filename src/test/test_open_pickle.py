dfc2177a1539ce20c3b9a696b445e3a2de90d02f
partizand <noemail@gmail.com>
2017-02-09 15:51:53 +0300

    тесты работают

--- src/test/test_open_pickle.py
+++ src/test/test_open_pickle.py
@@ -1,12 +1,15 @@
 import unittest
 
 from test.baseopentest import BaseOpenTest
-from test.basepickle import BasePickle
 
 
-class PickleOpenTest(BasePickle, BaseOpenTest, unittest.TestCase):
+class PickleOpenTest(unittest.TestCase, BaseOpenTest):
 
     test_name = 'test_open_pickle'
+
+    def __init__(self, *args, **kwargs):
+        super(PickleOpenTest, self).__init__(*args, **kwargs)
+        self.open_pickle()
 
 if __name__ == '__main__':
     unittest.main()
--- src/test/test_open_sql.py
+++ src/test/test_open_sql.py
@@ -8,13 +8,14 @@ class SQLOpenTest(unittest.TestCase, BaseOpenTest):
     Тестирование чтения данных из sql
     """
 
-    bookfile = "u:/sqllite_book/real-2017-01-26.gnucash"
+    # bookfile = "u:/sqllite_book/real-2017-01-26.gnucash"
 
     test_name = 'open_sql_test'
 
     def __init__(self, *args, **kwargs):
         super(SQLOpenTest, self).__init__(*args, **kwargs)
-        self.rep.open_book_sql(self.bookfile, open_if_lock=True)
+        self.open_sql()
+        # self.rep.open_book_sql(self.bookfile_sql, open_if_lock=True)
 
 if __name__ == '__main__':
     unittest.main()
