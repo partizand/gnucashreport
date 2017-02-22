ea7f3c5cde53bf14f427604ed67a6fbeae6ef109
partizand <noemail@gmail.com>
2017-02-22 14:18:37 +0300

    public script

---
+++ public.bat
@@ -0,0 +1,10 @@
+rem register project on PyPi
+rem Only once need!!!!
+
+call ".pycharmrc.bat"
+
+python setup.py sdist bdist_wheel --universal
+
+twine.exe register dist/gnucashreport-0.1.0-py2.py3-none-any.whl -r pypitest
+
+twine upload dist/* -r pypitest
