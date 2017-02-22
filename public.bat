7140b3aa0ce892d6be8e5fd35409e119d1581d0b
partizand <noemail@gmail.com>
2017-02-22 16:10:19 +0300

    public to PyPi

--- public.bat
+++ public.bat
@@ -5,6 +5,6 @@ call ".pycharmrc.bat"
 
 python setup.py sdist bdist_wheel --universal
 
-twine.exe register dist/gnucashreport-0.1.0-py2.py3-none-any.whl -r pypitest
+twine.exe register dist/gnucashreport-0.1.0-py2.py3-none-any.whl
 
-twine upload dist/* -r pypitest
+twine upload dist/*
