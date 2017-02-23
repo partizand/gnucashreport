de974778af56fa31c13d07d904728f5cee444199
partizand <noemail@mail.com>
2017-02-23 15:03:28 +0300

    version find update

--- setup.py
+++ setup.py
@@ -15,23 +15,16 @@ from setuptools import setup, find_packages
 
 
 # find version in init file
-def read(*names, **kwargs):
-    with io.open(
-        os.path.join(os.path.dirname(__file__), *names),
-        encoding=kwargs.get("encoding", "utf8")
-    ) as fp:
-        return fp.read()
+def find_version(filename):
+    with open(filename, 'r') as f:
+        version_file = f.read()
+        version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                                  version_file, re.M)
+        if version_match:
+            return version_match.group(1)
+        raise RuntimeError("Unable to find version string.")
 
-
-def find_version(*file_paths):
-    version_file = read(*file_paths)
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
-
-version = find_version("src/gnucashreport", "__init__.py")
+version = find_version("src/gnucashreport/__init__.py")
 
 with open('README.rst', encoding='utf-8') as f:
     long_description = f.read()
