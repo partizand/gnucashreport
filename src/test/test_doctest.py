addacc2f75a5dd80688bc8a7aedb3656d2262806
partizand <noemail@gmail.com>
2017-03-03 11:04:32 +0300

    simple xirr function

--- src/test/test_doctest.py
+++ src/test/test_doctest.py
@@ -7,11 +7,13 @@ import doctest
 
 import gnucashreport
 import gnucashreport.gnucashreport
+import gnucashreport.financial
 
 
 def load_tests(*args, **kwargs):
     test_all_doctests = unittest.TestSuite()
     test_all_doctests.addTest(doctest.DocTestSuite(gnucashreport.gnucashreport))
+    test_all_doctests.addTest(doctest.DocTestSuite(gnucashreport.financial))
     # test_all_doctests.addTest(doctest.testfile('D:\\Documents\\programming\\gcreports\\readme.rst'))
     return test_all_doctests
 
