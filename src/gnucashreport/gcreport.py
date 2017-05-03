0c229812b37c825756dffbc035e178bf433161ce
partizand <noemail@gmail.com>
2017-05-03 14:43:49 +0300

    tests is ok. Не проходит только тест Loans multi в report test

--- src/gnucashreport/gcreport.py
+++ src/gnucashreport/gcreport.py
@@ -21,14 +21,14 @@ class GCReport(GNUCashData):
 
     Example
 
-    >>> import gnucashreport
-    >>> gcrep = GCReport()
+    # >>> import gnucashreport
+    # >>> gcrep = GCReport()
 
     open sql or xml book
-    >>> gcrep.open_book_file('v:/gnucash-base/sqlite/GnuCash-base.gnucash', open_if_lock=True)
+    # >>> gcrep.open_book_file('v:/gnucash-base/sqlite/GnuCash-base.gnucash', open_if_lock=True)
 
     save reports by years in xlsx file
-    >>> gcrep.all_reports_excel('v:/tables/ex-test.xlsx', glevel=1)
+    # >>> gcrep.all_reports_excel('v:/tables/ex-test.xlsx', glevel=1)
 
     """
 
