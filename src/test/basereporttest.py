2595a90475efca8b7f6f3fc673e17fb188f29b05
partizand <elegaunte@gmail.com>
2018-02-08 12:27:42 +0300

    test rebase

--- src/test/basereporttest.py
+++ src/test/basereporttest.py
@@ -1,8 +1,12 @@
 import datetime
 
+from gnucashreport.gnucashbook import GNUCashBook
 from gnucashreport.gnucashdata import GNUCashData
+
 from gnucashreport.xlsxreport import XLSXReport
 from test.basetest import BaseTest
+
+from test.testinfo import TestInfo
 
 
 class BaseReportTest(BaseTest):
@@ -11,7 +15,187 @@ class BaseReportTest(BaseTest):
     All testing data on external resource because it is real data
     """
 
+    PICKLE_ASSETS = 'assets.pkl'
+    PICKLE_LOANS = 'loans.pkl'
+    PICKLE_EXPENSE = 'expense.pkl'
+    PICKLE_INCOME = 'income.pkl'
+    PICKLE_PROFIT = 'profit.pkl'
+    PICKLE_EQUITY = 'equity.pkl'
+    PICKLE_INFLATION = 'inflation.pkl'
+
     rep = GNUCashData()
+
+    #--------------------
+    # base functions
+
+    @classmethod
+    def save_testdata(cls):
+        """
+        Запись тестовых pickle для последующей проверки в тестах
+        :return:
+        """
+
+        # cls.open_sql()
+
+        cls.rep.open_book_file(TestInfo.BOOKFILE_SQL)
+
+        # save reports
+        # Assets
+        df = cls.get_assets()
+        cls._dataframe_to_pickle(df, filename=cls.pickle_assets, folder=cls.dir_testdata)
+        # Assets multi
+        df = cls.get_assets(glevel=cls.test_glevel2)
+        filename = cls._add_suffix(cls.pickle_assets, cls.test_level2_suffix)
+        cls._dataframe_to_pickle(df, filename=filename, folder=cls.dir_testdata)
+
+        # Loans
+        df = cls.get_loans()
+        cls._dataframe_to_pickle(df, filename=cls.pickle_loans, folder=cls.dir_testdata)
+        # Loans multi
+        df = cls.get_loans(glevel=cls.test_glevel2)
+        filename = cls._add_suffix(cls.pickle_loans, cls.test_level2_suffix)
+        cls._dataframe_to_pickle(df, filename=filename, folder=cls.dir_testdata)
+
+        # Expense
+        filename = cls.pickle_expense
+        df = cls.get_expense()
+        cls._dataframe_to_pickle(df, filename=filename, folder=cls.dir_testdata)
+        # Expense multi
+        df = cls.get_expense(glevel=cls.test_glevel2)
+        filename = cls._add_suffix(filename, cls.test_level2_suffix)
+        cls._dataframe_to_pickle(df, filename=filename, folder=cls.dir_testdata)
+
+        # Income
+        filename = cls.pickle_income
+        df = cls.get_income()
+        cls._dataframe_to_pickle(df, filename=filename, folder=cls.dir_testdata)
+        # Income multi
+        df = cls.get_income(glevel=cls.test_glevel2)
+        filename = cls._add_suffix(filename, cls.test_level2_suffix)
+        cls._dataframe_to_pickle(df, filename=filename, folder=cls.dir_testdata)
+
+        # Profit
+        filename = cls.pickle_profit
+        df = cls.get_profit()
+        cls._dataframe_to_pickle(df, filename=filename, folder=cls.dir_testdata)
+        # Profit multi
+        df = cls.get_profit(glevel=cls.test_glevel2)
+        filename = cls._add_suffix(filename, cls.test_level2_suffix)
+        cls._dataframe_to_pickle(df, filename=filename, folder=cls.dir_testdata)
+
+        # Equity
+        filename = cls.pickle_equity
+        df = cls.get_equity()
+        cls._dataframe_to_pickle(df, filename=filename, folder=cls.dir_testdata)
+        # Equity multi
+        df = cls.get_equity(glevel=cls.test_glevel2)
+        filename = cls._add_suffix(filename, cls.test_level2_suffix)
+        cls._dataframe_to_pickle(df, filename=filename, folder=cls.dir_testdata)
+
+        # Inflation annual
+        filename = cls.pickle_inflation
+        df = cls.get_inflation(cumulative=False)
+        cls._dataframe_to_pickle(df, filename=filename, folder=cls.dir_testdata)
+        # Inflation cumulative
+        df = cls.get_inflation(cumulative=True)
+        filename = cls._add_suffix(filename, cls.test_level2_suffix)
+        cls._dataframe_to_pickle(df, filename=filename, folder=cls.dir_testdata)
+
+        # save sql to pickle book
+        cls.rep._read_book_sql(BaseTest.bookfile_sql)  # Получаем сырые данные
+        cls._save_db_to_pickle(folder=cls.dir_testdata)
+
+        # save xml to pickle book
+        cls.rep._read_book_xml(BaseTest.bookfile_xml)  # Вроде по тестам перезапись данных и все ок
+        cls._save_db_to_pickle(folder=cls.dir_testdata, suffix='-xml')
+
+    @classmethod
+    def get_assets(cls, glevel=None):
+        """
+        Get assets dataframe for testing or saving
+        :return:
+        """
+        if not glevel:
+            glevel = cls.test_glevel
+        df = cls.rep.balance_by_period(from_date=cls.test_from_date, to_date=cls.test_to_date,
+                                       period=cls.test_period, glevel=glevel)
+        return df
+
+    @classmethod
+    def get_loans(cls, glevel=None):
+        """
+        Get assets dataframe for testing or saving
+        :return:
+        """
+        if not glevel:
+            glevel = 0
+        df = cls.rep.balance_by_period(from_date=cls.test_from_date, to_date=cls.test_to_date,
+                                       account_types=[GNUCashBook.LIABILITY], period=cls.test_period, glevel=glevel)
+        return df
+
+    @classmethod
+    def get_expense(cls, glevel=None):
+        """
+        Get assets dataframe for testing or saving
+        :return:
+        """
+        if not glevel:
+            glevel = cls.test_glevel
+        df = cls.rep.turnover_by_period(from_date=cls.test_from_date, to_date=cls.test_to_date,
+                                        account_type=GNUCashBook.EXPENSE,
+                                        glevel=glevel)
+        return df
+
+    @classmethod
+    def get_income(cls, glevel=None):
+        """
+        Get assets dataframe for testing or saving
+        :return:
+        """
+        if not glevel:
+            glevel = cls.test_glevel
+        df = cls.rep.turnover_by_period(from_date=cls.test_from_date, to_date=cls.test_to_date,
+                                        account_type=GNUCashBook.INCOME,
+                                        glevel=glevel)
+        return df
+
+    @classmethod
+    def get_profit(cls, glevel=None):
+        """
+        Get assets dataframe for testing or saving
+        :return:
+        """
+        if not glevel:
+            glevel = cls.test_glevel
+        df = cls.rep.profit_by_period(from_date=cls.test_from_date, to_date=cls.test_to_date,
+                                      glevel=glevel)
+        return df
+
+    @classmethod
+    def get_equity(cls, glevel=None):
+        """
+        Get assets dataframe for testing or saving
+        :return:
+        """
+        if not glevel:
+            glevel = cls.test_glevel
+        df = cls.rep.equity_by_period(from_date=cls.test_from_date, to_date=cls.test_to_date,
+                                      glevel=glevel)
+        return df
+
+    @classmethod
+    def get_inflation(cls, cumulative=False, glevel=None):
+        """
+        Get assets dataframe for testing or saving
+        :return:
+        """
+        if not glevel:
+            glevel = cls.test_glevel
+        df = cls.rep.inflation_by_period(from_date=cls.test_from_date_y, to_date=cls.test_to_date_y, period='A',
+                                         glevel=glevel, cumulative=cumulative)
+        return df
+
+    #---------------------
 
     def test_assets(self):
         df = self.get_assets()
---
+++ src/test/testinfo.py
@@ -0,0 +1,46 @@
+from datetime import date
+
+class TestInfo:
+
+    # Данные для генерации тестовых данных и тестирования
+    # Test info, change before generate test data!
+    BOOKFILE_SQL = 'v:/gnucash-base/sqlite/GnuCash-base.gnucash'
+    BOOKFILE_XML = 'v:/gnucash-base/xml/GnuCash-base.gnucash'
+    DIR_TESTDATA = 'v:/test_data'
+    GNUCASH_TESTBASE_XML = 'data/xirr-test.gnucash'
+    GNUCASH_TESTBASE_SQL = 'data/xirr-test-sql.gnucash'
+    # end folder options---------------------------------------
+    TEST_FROM_DATE = date(2016, 1, 1)
+    TEST_TO_DATE = date(2016, 12, 31)
+    TEST_FROM_DATE_Y = date(2009, 1, 1)
+    TEST_TO_DATE_Y = date(2016, 12, 31)
+    TEST_PERIOD = 'M'
+    TEST_GLEVEL = 1
+    TEST_GLEVEL2 = [0, 1]
+    TEST_LEVEL2_SUFFIX = '-2'
+    # end test info--------------------------------------------
+
+    # rep = GNUCashData()
+
+
+    # dir_testdata = GNUCashData.dir_testdata
+
+    # pickle_prices = 'prices.pkl'
+    # pickle_splits = 'splits.pkl'
+    # pickle_accounts = 'accounts.pkl'
+    # pickle_tr = 'transactions.pkl'
+    # pickle_commodities = 'commodities.pkl'
+
+    pickle_prices = GNUCashData.pickle_prices  # 'prices.pkl'
+    pickle_splits = GNUCashData.pickle_splits  # 'splits.pkl'
+    pickle_accounts = GNUCashData.pickle_accounts  # 'accounts.pkl'
+    pickle_tr = GNUCashData.pickle_tr  # 'transactions.pkl'
+    pickle_commodities = GNUCashData.pickle_commodities  # 'commodities.pkl'
+
+    PICKLE_ASSETS = 'assets.pkl'
+    PICKLE_LOANS = 'loans.pkl'
+    PICKLE_EXPENSE = 'expense.pkl'
+    PICKLE_INCOME = 'income.pkl'
+    PICKLE_PROFIT = 'profit.pkl'
+    PICKLE_EQUITY = 'equity.pkl'
+    PICKLE_INFLATION = 'inflation.pkl'
