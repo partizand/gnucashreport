321b9ae1a1501e4a38b154df84f2c5bdc5f13625
partizand <elegaunte@gmail.com>
2018-02-09 13:14:23 +0300

    GnuCashbook_test change. Test is ok

--- src/test/test_gnucashbook.py
+++ src/test/test_gnucashbook.py
@@ -20,7 +20,7 @@ class GnuCashBook_Test(unittest.TestCase):
     """
 
     # Список полей загруженных dataframe для проверки
-    account_fields = [cols.SHORTNAME, cols.ACCOUNT_TYPE,
+    account_fields = [cols.SHORTNAME, cols.FULLNAME, cols.ACCOUNT_TYPE,
                       cols.COMMODITY_GUID, "commodity_scu",
                       "parent_guid", "description", "hidden", cols.NOTES]
     comm_fields = ["namespace", "mnemonic"]
@@ -29,21 +29,22 @@ class GnuCashBook_Test(unittest.TestCase):
                     "memo", "reconcile_state", "value", "quantity"]
     price_fields = ["currency_guid", "source", "type", "value"]
 
-    test_name = 'abstract_open_test'
+    # test_name = 'abstract_open_test'
 
     # book = GNUCashBook()
 
     @classmethod
     def setUpClass(cls):
-        gcrep_xml = GNUCashBook()
-        gcrep_sql = GNUCashBook()
-        base_path = os.path.dirname(os.path.realpath(__file__))
-        xml_book = os.path.join(base_path, TestInfo.GNUCASH_TESTBASE_XML)
-        sql_book = os.path.join(base_path, TestInfo.GNUCASH_TESTBASE_SQL)
-        gcrep_xml.open_file(xml_book)
-        gcrep_sql.open_file(sql_book)
+        # gcrep_xml = GNUCashBook()
+        # gcrep_sql = GNUCashBook()
+        # base_path = os.path.dirname(os.path.realpath(__file__))
+        # xml_book = os.path.join(base_path, TestInfo.GNUCASH_TESTBASE_XML)
+        # sql_book = os.path.join(base_path, TestInfo.GNUCASH_TESTBASE_SQL)
+        # gcrep_xml.open_file(xml_book)
+        # gcrep_sql.open_file(sql_book)
 
-        cls.test_array = [('xml', gcrep_xml), ('sql', gcrep_sql)]
+        # cls.test_array = [('xml', gcrep_xml), ('sql', gcrep_sql)]
+        cls.test_array = [GNUCashBook(TestInfo.GNUCASH_TESTBASE_XML), GNUCashBook(TestInfo.GNUCASH_TESTBASE_SQL)]
 
     # @classmethod
     # def open_book(cls, filename):
@@ -56,77 +57,77 @@ class GnuCashBook_Test(unittest.TestCase):
         Проверка имен столбцов прочитанных данных
         :return:
         """
-        for test_name, book in self.test_array:
-            with self.subTest(test_name):
-                self.dataframe_fields_control(book.df_accounts, self.account_fields, 'df_account sql ' + self.test_name)
-                self.dataframe_fields_control(book.df_commodities, self.comm_fields, 'df_commodities sql ' + self.test_name)
-                self.dataframe_fields_control(book.df_transactions, self.tr_fields, 'df_transactions sql ' + self.test_name)
-                self.dataframe_fields_control(book.df_prices, self.price_fields, 'df_prices sql ' + self.test_name)
-                self.dataframe_fields_control(book.df_splits, self.split_fields, 'df_splits sql ' + self.test_name)
+        for book in self.test_array:
+            with self.subTest(book):
+                self.dataframe_fields_control(book.df_accounts, self.account_fields, 'df_account sql ')
+                self.dataframe_fields_control(book.df_commodities, self.comm_fields, 'df_commodities sql ')
+                self.dataframe_fields_control(book.df_transactions, self.tr_fields, 'df_transactions sql ')
+                self.dataframe_fields_control(book.df_prices, self.price_fields, 'df_prices sql ')
+                self.dataframe_fields_control(book.df_splits, self.split_fields, 'df_splits sql ')
 
     def test_splits_value_decimal(self):
-        for test_name, book in self.test_array:
-            with self.subTest(test_name):
+        for book in self.test_array:
+            with self.subTest(book):
                 value = book.df_splits[cols.VALUE][0]
                 self.assertIsInstance(value, decimal.Decimal, 'Splits value is decimal type')
 
     def test_splits_quantity_decimal(self):
-        for test_name, book in self.test_array:
-            with self.subTest(test_name):
+        for book in self.test_array:
+            with self.subTest(book):
                 value = book.df_splits[cols.QUANTITY][0]
                 self.assertIsInstance(value, decimal.Decimal, 'Splits quantity is decimal type')
 
     def test_prices_value_decimal(self):
-        for test_name, book in self.test_array:
-            with self.subTest(test_name):
+        for book in self.test_array:
+            with self.subTest(book):
                 value = book.df_prices[cols.VALUE][0]
                 self.assertIsInstance(value, decimal.Decimal, 'Prices value is decimal type')
 
     def test_accounts_notempty(self):
-        for test_name, book in self.test_array:
-            with self.subTest(test_name):
+        for book in self.test_array:
+            with self.subTest(book):
                 self.df_notempty(book.df_accounts, 'df_accounts')
 
     def test_splits_notempty(self):
-        for test_name, book in self.test_array:
-            with self.subTest(test_name):
+        for book in self.test_array:
+            with self.subTest(book):
                 self.df_notempty(book.df_splits, 'df_splits')
 
     def test_accounts_index(self):
-        for test_name, book in self.test_array:
-            with self.subTest(test_name):
+        for book in self.test_array:
+            with self.subTest(book):
                 self.assertEqual(book.df_accounts.index.name, cols.GUID,'df_accounts index is not GUID')
 
     def test_splits_index(self):
-        for test_name, book in self.test_array:
-            with self.subTest(test_name):
+        for book in self.test_array:
+            with self.subTest(book):
                 self.assertEqual(book.df_splits.index.name, cols.GUID,'df_splits index is not GUID')
 
     def test_transactions_index(self):
-        for test_name, book in self.test_array:
-            with self.subTest(test_name):
+        for book in self.test_array:
+            with self.subTest(book):
                 self.assertEqual(book.df_transactions.index.name, cols.GUID,'df_transactions index is not GUID')
 
     def test_prices_index(self):
-        for test_name, book in self.test_array:
-            with self.subTest(test_name):
+        for book in self.test_array:
+            with self.subTest(book):
                 self.assertEqual(book.df_prices.index.name, cols.GUID, 'df_prices index is not GUID')
 
     def test_commodities_index(self):
-        for test_name, book in self.test_array:
-            with self.subTest(test_name):
+        for book in self.test_array:
+            with self.subTest(book):
                 self.assertEqual(book.df_commodities.index.name, cols.GUID,'df_commodities index is not GUID')
 
     def df_notempty(self, df, name_df):
-        for test_name, book in self.test_array:
-            with self.subTest(test_name):
+        for book in self.test_array:
+            with self.subTest(book):
                 self.assertIsNotNone(df, '{} is not none'.format(name_df))
                 l = len(df)
                 self.assertGreater(l, 0, '{} contain lines'.format(name_df))
 
     def test_rootaccount_filled(self):
-        for test_name, book in self.test_array:
-            with self.subTest(test_name):
+        for book in self.test_array:
+            with self.subTest(book):
                 self.assertIsNotNone(book.root_account_guid, 'root_account_guid is not none')
                 self.assertNotEqual(book.root_account_guid, '', 'root_account_guid is not empty')
 
@@ -140,6 +141,6 @@ class GnuCashBook_Test(unittest.TestCase):
         """
         cols = df.columns.values.tolist()
         for field in etalon_fields:
-            self.assertIn(field, cols, 'DataFrame {} contain field {}. {}'.format(df_name, field, self.test_name))
+            self.assertIn(field, cols, 'DataFrame {} contain field {}.'.format(df_name, field))
 
 
