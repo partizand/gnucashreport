b89cd0aa00729e09478c5580953d2f69cbf882da
partizand <elegaunte@gmail.com>
2018-02-16 10:46:00 +0300

    Add all doctest to unit All test is passed!

--- src/test/test_doctest.py
+++ src/test/test_doctest.py
@@ -6,16 +6,20 @@ import unittest
 import doctest
 
 import gnucashreport
-import gnucashreport.gcreport
+# import gnucashreport.gcreport
+import gnucashreport.xlsxreport
 import gnucashreport.financial
 import gnucashreport.utils
+import gnucashreport.reportset
 
 
 def load_tests(*args, **kwargs):
     test_all_doctests = unittest.TestSuite()
     # test_all_doctests.addTest(doctest.DocTestSuite(gnucashreport.gcreport))
+    test_all_doctests.addTest(doctest.DocTestSuite(gnucashreport.xlsxreport))
     test_all_doctests.addTest(doctest.DocTestSuite(gnucashreport.financial))
     test_all_doctests.addTest(doctest.DocTestSuite(gnucashreport.utils))
+    test_all_doctests.addTest(doctest.DocTestSuite(gnucashreport.reportset))
     # test_all_doctests.addTest(doctest.testfile('D:\\Documents\\programming\\gcreports\\readme.rst'))
     return test_all_doctests
 
