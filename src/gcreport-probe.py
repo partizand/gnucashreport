8a31a26d95773f69af81b4a541c9e17f9de1ccde
partizand <elegaunte@gmail.com>
2018-02-16 16:05:02 +0300

    info update

--- src/gcreport-probe.py
+++ src/gcreport-probe.py
@@ -17,9 +17,6 @@ bookfile_xml = 'v:/gnucash-base/xml/GnuCash-base.gnucash'
 
 out_filename = 'v:/tables/ex-test.xlsx'
 
-# Unknown multi transaction for xirr calculate. Transaction info: guid=3723d09d9b1d0a555bbc8bb6a57a1cb3. Date=2009-07-12 00:00:00. Descr=None
-# Unknown multi transaction for xirr calculate. Transaction info: guid=3723d09d9b1d0a555bbc8bb6a57a1cb3. Date=2009-07-12 00:00:00. Descr=
-
 
 # pie_book = piecash.open_book(bookfile_sql)
 
@@ -31,9 +28,7 @@ out_filename = 'v:/tables/ex-test.xlsx'
 # BuildReports
 raw_data = RawData(bookfile_sql)
 
-print ('3723d09d9b1d0a555bbc8bb6a57a1cb3' == '3723d09d9b1d0a555bbc8bb6a57a1cb3')
 
-df_splits = raw_data.df_splits[raw_data.df_splits[cols.TRANSACTION_GUID] == '3723d09d9b1d0a555bbc8bb6a57a1cb3']
 # account_guids = df_splits[cols.ACCOUNT_GUID]
 # df_accounts = raw_data.df_accounts[raw_data.df_accounts[cols.GUID] in account_guids]
 with XLSXReport('v:/tables/df_splits.xlsx') as outputer_excel:
