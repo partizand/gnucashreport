92772e115c9a89cb1586bf59009d3ed0a37b8b5b
partizand <elegaunte@gmail.com>
2018-06-07 14:52:24 +0300

    Add cli test

---
+++ src/test/test_cli.py
@@ -0,0 +1,22 @@
+import unittest
+
+from gnucashreport.gcreportcli import build_cli_report, parse_args
+
+from test.testinfo import TestInfo
+
+class CliProg_Test(unittest.TestCase):
+
+    def test_parse_args(self):
+        gnucash_file = 'my.gnucash'
+        xlsx_file = 'my.xlsx'
+        args = [gnucash_file, xlsx_file]
+        p_args = parse_args(args)
+        self.assertEqual(gnucash_file, p_args.gnucash_file, 'gnucash file arg parsing')
+        self.assertEqual(xlsx_file, p_args.xlsx_file, 'xlsx file arg parsing')
+        self.assertEqual(1, p_args.glevel, 'glevel arg parsing')
+
+    def test_cli_build_report(self):
+        gnucash_file = 'v:/gnucash-base/sqlite/GnuCash-base.gnucash'
+        xlsx_file = 'v:/tables/ex-test.xlsx'
+        build_cli_report(gnucash_file, xlsx_file, 1)
+        print('end build')
