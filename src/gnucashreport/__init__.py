cc00915d490cb810143ec98e2c964d13636c5105
partizand <partizand@gmail.com>
2019-11-08 15:21:18 +0400

    update ver to 0.2.5

--- src/gnucashreport/__init__.py
+++ src/gnucashreport/__init__.py
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 # from .gcreport import GCReport
 
-__version__ = '0.2.4'
+__version__ = '0.2.5'
 __author__ = "Partizand"
 __license__ = "GPL3"
 
